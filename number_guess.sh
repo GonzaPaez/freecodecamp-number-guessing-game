e1733c1c8949f161849527c79f1c0604e3818d86
Gonzalo Paez <129911550+GonzaPaez@users.noreply.github.com>
2025-11-13 15:23:37 -0500

    feat: Check if the user exists in the database

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+read -p "Enter your username: " USER_NAME
+
+if [[ -z $USER_NAME ]]
+then
+  echo -e "\nPlease enter your name\n"
+  exit
+fi
+
+USER_IN_DATABASE=$($PSQL "SELECT * FROM users WHERE user_name='$USER_NAME'")
+
+if [[ -z $USER_IN_DATABASE ]]
+then
+  echo -e "\nWelcome, $USER_NAME! It looks like this your first time here."
+else
+  IFS="|" read USER_ID, USER_NAME GAMES_PLAYED BEST_GAME <<< "$USER_IN_DATABASE"
+  echo -e "\nWelcome back, $USER_NAME! You have played $GAMES_PLAYED game, and your best game took $BEST_GAME guesses."
+fi

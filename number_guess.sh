fb232883085f0cb0b0adfd66e0c1598b66533b08
Gonzalo Paez <129911550+GonzaPaez@users.noreply.github.com>
2025-11-13 16:10:58 -0500

    feat: Insert the user's data into the database

--- number_guess.sh
+++ number_guess.sh
@@ -16,7 +16,7 @@ if [[ -z $USER_IN_DATABASE ]]
 then
   echo -e "\nWelcome, $USER_NAME! It looks like this your first time here."
 else
-  IFS="|" read USER_ID, USER_NAME GAMES_PLAYED BEST_GAME <<< "$USER_IN_DATABASE"
+  IFS="|" read USER_ID USER_NAME GAMES_PLAYED BEST_GAME <<< "$USER_IN_DATABASE"
   echo -e "\nWelcome back, $USER_NAME! You have played $GAMES_PLAYED game, and your best game took $BEST_GAME guesses."
 fi
 
@@ -44,3 +44,11 @@ do
     break
   fi
 done
+
+if [[ -z $USER_IN_DATABASE ]]
+then
+  INSERT_USER=$($PSQL "INSERT INTO users (user_name, games_played, best_game) VALUES ('$USER_NAME', 1, $NUMBER_OF_GUESSES)")
+else
+  UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE user_name='$USER_NAME'")
+  UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game = LEAST(best_game, $NUMBER_OF_GUESSES) WHERE user_name='$USER_NAME'")
+fi

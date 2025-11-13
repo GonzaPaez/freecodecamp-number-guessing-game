ea47be699e8e0d9e040cab287dcfeda0446852f0
Gonzalo Paez <129911550+GonzaPaez@users.noreply.github.com>
2025-11-13 15:38:26 -0500

    feat: Create a loop until the user guesses the secret number

--- number_guess.sh
+++ number_guess.sh
@@ -23,3 +23,24 @@ fi
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 NUMBER_OF_GUESSES=0
 
+echo "Guess the secret number between 1 and 1000:"
+
+while true
+do
+  read GUESS
+  ((NUMBER_OF_GUESSES++))
+
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  elif (( GUESS < SECRET_NUMBER ))
+  then
+    echo "It's higher than that, guess again:"
+  elif (( GUESS > SECRET_NUMBER ))
+  then
+    echo "It's lower than that, guess again:"
+  else
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    break
+  fi
+done
